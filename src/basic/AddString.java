661cc94f85e8c0e156078cfa9c5d6f8d639fe1bf
Nipa <Nipa@10.67.109.22>
2021-05-15 16:38:24 -0400

    first commit

---
+++ src/basic/AddString.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class AddString {
+
+	public static void main(String[] args) {
+		String name;
+		name=" nipa ";
+		System.out.print("Hello,My Name is" +name);
+
+	}//end of main
+	
+
+}//end of AddString
